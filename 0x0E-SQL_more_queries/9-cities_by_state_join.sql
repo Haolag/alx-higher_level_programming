f8652ef5d7c5b0122063ac75fd9f4e34675de56c
HaolagTechConsult <hakeemgbajabiamila.info@gmail.com>
2022-11-29 17:56:10 +0100

    my sql more queries files

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
