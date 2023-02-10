d7b791dd8fedfa215ff3dbb64ac9b2934db418e0
HaolagTechConsult <hakeemgbajabiamila.info@gmail.com>
2023-02-10 20:00:49 +0100

    Delete 0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const url = process.argv[2];
-request
-  .get(url)
-  .on('response', function (response) {
-    console.log('code: ' + response.statusCode);
-  });
